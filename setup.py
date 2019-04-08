232bc14554091b788a4a0f4fa26e86a1dd93e021
Rasmus Munk <munk1@live.dk>
2019-04-08 15:01:52 +0200

    Update setup.py to be complient with check rms

--- setup.py
+++ setup.py
@@ -7,15 +7,30 @@ version_ns = {}
 with open(os.path.join(cur_dir, 'version.py')) as f:
     exec(f.read(), {}, version_ns)
 
+long_description = open('README.rst').read()
 setup(
     name='ldap_hooks',
     version=version_ns['__version__'],
-    description="""
-    """,
+    long_description=long_description,
+    author="Rasmus Munk",
+    author_email="munk1@live.dk",
     packages=find_packages(),
+    url="https://github.com/rasmunk/ldap_hooks",
+    license="MIT",
+    keywords=['Web', 'JupyterHub', 'Spawner', 'Hook'],
     install_requires=[
         'ldap3>=2.5.2',
         'traitlets>=4.3.2',
         'gen>=0.1'
-    ]
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
