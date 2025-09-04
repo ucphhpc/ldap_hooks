8fe5893a78b434911659c44466d4c3d854b8e0b9
Rasmus Munk <rasmunk@users.noreply.github.com>
2025-09-04 15:49:36 +0200

    Update the GoCD config

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
